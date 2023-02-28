2758ea896a8da0d039bca4655eab97e0267bf267
Ivan Hofer <ivan.hofer@outlook.com>
2023-02-28 10:45:11 +0100

    update all dependencies

--- build.js
+++ build.js
@@ -1,32 +1,32 @@
-/**
- * This is the build script for the project.
- *
- * It takes the source code and bundles it into a single file
- * that can be imported into an inlang project.
- */
-
-import { build } from "esbuild";
+import { context } from 'esbuild'
 import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
 
-await build({
+const ctx = await context({
   entryPoints: ["src/index.ts"],
   outdir: "dist",
   bundle: true,
-  // minification is disabled in dev mode for better debugging
-  minify: process.env.DEV ? false : true,
-  watch: process.env.DEV ? true : false,
+  minify: !process.env.DEV,
   format: "esm",
   platform: "browser",
   target: "es2020",
   plugins: [
-    // by default node polyfills are included
-    // as a lot of npm packages that deal with files
-    // use built-in node modules
     NodeModulesPolyfillPlugin(),
+    {
+      name: 'logger',
+      setup: ({ onEnd }) => onEnd(() => console.info('🎉 changes processed'))
+    }
   ],
 });
 
-console.log("✅ build complete");
 if (process.env.DEV) {
-  console.log("👀 watching for changes...");
+  await ctx.watch()
+  console.info('👀 watching for changes...')
+  process.on('exit', async () => {
+    console.info('🙈 process killed')
+    await ctx.dispose()
+  })
+} else {
+  await ctx.rebuild()
+  console.info('✅ build complete')
+  await ctx.dispose()
 }
